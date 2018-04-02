14e1423ee57589011880a79e3747730c5c6d1b02
Yann Solol <37675007+maxx6262@users.noreply.github.com>
2018-04-02 06:33:08 +0200

    Ajout parties multiples
    Nouvelle variable : int continuerPartie mise à jour à chaque fin de partue

--- main.c
+++ main.c
@@ -5,58 +5,60 @@
 #include <time.h>
 
 
-int main ( int argc, char** argv )
+int main ( int argc, char** argv ) {
+        int continuerPartie = 1;
+    do {
 
-{
+        int nombreMystere = 0, nombreEntre = 0;
 
-    int nombreMystere = 0, nombreEntre = 0;
+        int nombreCoups = 0;
 
-    int nombreCoups = 0;
-
-    const int MAX = 100, MIN = 1;
-
-
-    // Génération du nombre aléatoire
+        const int MAX = 100, MIN = 1;
 
 
-    srand(time(NULL));
-
-    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+        // Génération du nombre aléatoire
 
 
-    /* La boucle du programme. Elle se répète tant que l'utilisateur n'a pas trouvé le nombre mystère */
+        srand(time(NULL));
+
+        nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
 
 
-    do
-
-    {
-
-        // On demande le nombre
-
-        printf("Quel est le nombre ? ");
-
-        scanf("%d", &nombreEntre);
-        nombreCoups++;
-
-        // On compare le nombre entré avec le nombre mystère
+        /* La boucle du programme. Elle se répète tant que l'utilisateur n'a pas trouvé le nombre mystère */
 
 
-        if (nombreMystere > nombreEntre)
+        do {
 
-            printf("C'est plus !\n\n");
+            // On demande le nombre
 
-        else if (nombreMystere < nombreEntre)
+            printf("Quel est le nombre ? ");
 
-            printf("C'est moins !\n\n");
+            scanf("%d", &nombreEntre);
+            nombreCoups++;
 
-        else
-
-            printf ("Bravo, vous avez trouve le nombre mystere en %d coups !!!\n\n", nombreCoups);
-
-    } while (nombreEntre != nombreMystere);
+            // On compare le nombre entré avec le nombre mystère
 
 
+            if (nombreMystere > nombreEntre)
+
+                printf("C'est plus !\n\n");
+
+            else if (nombreMystere < nombreEntre)
+
+                printf("C'est moins !\n\n");
+
+            else
+
+                printf("Bravo, vous avez trouve le nombre mystere en %d coups !!!\n\n", nombreCoups);
+
+        } while (nombreEntre != nombreMystere);
+
+            printf("Refaire une partie ?  0 pour Non \n ");
+            scanf("%d",&continuerPartie);
+    }
+    while (continuerPartie);
+
+    printf("\n Merci et au revoir !!! ");
 
     return 0;
-
 }
